2ca9924cd0b31ee43d563b2cf6bf3eb8c8034734
Vilmer Dahlberg (Work) <vilmer.dahlberg@gmail.com>
2022-11-07 10:39:46 +0100

    Generated figures for paper

---
+++ src/postprocess/plotDesign.m
@@ -0,0 +1,14 @@
+function fig = plotDesign(i_img, j_img, xx)
+sizey = max(i_img);
+sizex = max(j_img);
+fig = figure(...
+    'Position', [50, 50, sizex, sizey]/helem, ...
+    'MenuBar', 'none');
+ax = axes(fig, ...
+    'Position', [0, 0, 1, 1]);
+axis(ax, 'off');
+axis(ax, 'tight')
+hold(ax, 'on');
+dimg = sparse(i_img, j_img, xx);
+imagesc(ax, dimg);
+end
